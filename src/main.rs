59ef366c9a3b6141459c2ff1e4ac83e4254668bf
5-23 <yhanbyeol6@gmail.com>
2024-08-14 20:57:48 +0900

    2nd try

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,50 @@
-use reqwest::Client;
+use rand::seq::IteratorRandom;
 use reqwest::Proxy;
 use serde_json::json;
-use std::time::Duration;
-use tokio_socks::tcp::Socks5Stream;
-use url::Url;
 
 #[tokio::main]
 async fn main() {
-    let code = "3CE9mU3hDXC4c2Q6";
+    loop {
+        let code = code_gen();
 
-    let headers = json!({
-        // chrome
-        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) discord/0.0.314 Chrome/124.0.6367.243 Electron/30.2.0 Safari/537.36",
-        "timeout": 10000,
-        "validateStatus": null,
+        let client = reqwest::Client::new();
+        let res = client
+            .get(format!("https://discord.com/api/v9/entitlements/gift-codes/{code}?with_application=false&with_subscription_plan=true"))
+            .send().await.unwrap();
+        let res_json = res.json::<serde_json::Value>().await.unwrap();
+        match res_json["message"].as_str().unwrap() {
+            "You are being rate limited." => {}
+            "Unknown Gift Code" => {}
+            "This gift has been redeemed already." => {}
+            
+            "The resource is being rate limited." => {
+                println!("Rate limited");
+                std::thread::sleep(std::time::Duration::from_micros(
+                    (res_json["retry_after"].as_f64().unwrap() * 1000.0) as u64,
+                ));
+            }
+            "You are being blocked from accessing our API temporarily due to exceeding global rate limits. Refer to https://discord.com/developers/docs/topics/rate-limits for more information." => {
+                println!("Rate limited");
+                std::thread::sleep(std::time::Duration::from_micros(
+                    (res_json["retry_after"].as_f64().unwrap() * 1000.0) as u64,
+                ));
+            }
+            _ => {
+                println!("{}: {}", code, res_json);
+            }
+        }
+        std::thread::sleep(std::time::Duration::from_micros(
+            (0..2000).choose(&mut rand::thread_rng()).unwrap(),
+        ));
+    }
+}
 
-    });
-    let client = reqwest::Client::new();
-    let res = client
-        .get(format!("https://discord.com/api/v9/entitlements/gift-codes/{code}?with_application=false&with_subscription_plan=true"))
-        .json(&headers)
-        .send().await.unwrap();
-    println!("{:?}", res.text().await.unwrap());
+fn code_gen() -> String {
+    let code_rng = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    let mut rng = rand::thread_rng();
+    let mut code = String::new();
+    for i in 0..16 {
+        code.push(code_rng.chars().choose(&mut rng).unwrap());
+    }
+    code
 }
